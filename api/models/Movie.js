dc863709c664b503fa9f66c3b54344d7ca1b0fc7
Fredersen <naim.jhuboo@gmail.com>
2022-04-22 01:59:05 +0200

    list added with API

--- api/models/Movie.js
+++ api/models/Movie.js
@@ -1,41 +1,20 @@
 const mongoose = require('mongoose');
 
-const MovieSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        unique: true
+const MovieSchema = new mongoose.Schema(
+    {
+      title: { type: String, required: true, unique: true },
+      desc: { type: String },
+      img: { type: String },
+      imgTitle: { type: String },
+      imgSm: { type: String },
+      trailer: { type: String },
+      video: { type: String },
+      year: { type: String },
+      limit: { type: Number },
+      genre: { type: String },
+      isSeries: { type: Boolean, default: false },
     },
-    description: {
-        type: String
-    },
-    img: {
-        type: String
-    },
-    imgTitle: {
-        type: String
-    },
-    imgSm: {
-        type: String
-    },
-    trailer: {
-        type: String
-    },
-    video: {
-        type: String
-    },
-    year: {
-        type: Number
-    },
-    genre: {
-        type: String
-    },
-    isSeries: {
-        type: Boolean,
-        default: false
-    },
-}, {
-    timestamps: true
-});
+    { timestamps: true }
+  );
 
 module.exports = mongoose.model("Movie", MovieSchema);
